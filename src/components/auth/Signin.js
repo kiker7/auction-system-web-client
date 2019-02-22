fe43e68742c4e39bb0251d12523983913372e74d
rrutyna <rafalrutyna00@gmai.com>
2019-02-22 21:27:22 +0100

    Added Signin component.

--- src/components/auth/Signin.js
+++ src/components/auth/Signin.js
@@ -1,9 +1,81 @@
 import React from 'react';
+import {produce} from "immer";
+import PropTypes from 'prop-types';
+import SigninForm from './SigninForm';
+import {bindActionCreators} from "redux";
+import * as actions from "../../actions/userActions";
+import {connect} from "react-redux";
+import { toast } from 'react-toastify';
 
-const Signin = () => (
-  <div>
-    <h2>Sign in..</h2>
-  </div>
-);
+export class Signin extends React.Component{
 
-export default Signin;
+  state = {
+    user: {},
+    sending: false,
+    errors: {}
+  };
+
+
+  handleChange = event => {
+    const {name, value} = event.target;
+    this.setState(
+      produce(draft => {
+        draft.user[name] = value;
+      })
+    );
+  };
+
+  handleSave = event => {
+    event.preventDefault();
+    this.setState({sending: true});
+
+    this.props.actions
+      .signIn(this.state.user)
+      .then(() => {
+        console.log('Log in'); // eslint-disable-line no-console
+        this.setState({
+          sending: false,
+          errors: {}
+        });
+        toast.success("Successful login");
+        this.props.history.push("/");
+      })
+      .catch(error => {
+        this.setState({
+          sending: false,
+          errors: {onSave: error.message}
+        });
+      });
+  };
+
+  render() {
+    return (
+      <SigninForm
+        onSave={this.handleSave}
+        onChange={this.handleChange}
+        sending={this.state.sending}
+        errors={this.state.errors}
+      />
+    );
+  }
+}
+
+Signin.propTypes = {
+  actions: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    user: state.user
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Signin);
