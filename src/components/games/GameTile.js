2d40845d59e1e292ec812b9cb17df2c65da059a0
rrutyna <rafalrutyna00@gmai.com>
2019-03-14 23:49:13 +0100

    Style auction links.

--- src/components/games/GameTile.js
+++ src/components/games/GameTile.js
@@ -7,8 +7,11 @@ const GameTile = ({game, postAuction, checkGameOwner}) => (
     <div className="card-body">
       <h4 className="card-title">{game.name}</h4>
       <p>Price {game.price}</p>
-      {(game.hasOwnProperty('auction') && game.auction ) ? (<div><a href="#">Link to auction</a></div>)
-        : (checkGameOwner && (<div><a onClick={() => postAuction(game.id)}>Post auction - from owner</a></div>))}
+      {(game.hasOwnProperty('auction') && game.auction) ? (
+          <div className="text-center"><a href="#" className="btn btn-info auction-link text-center" role="button" aria-pressed="true">Go to auction</a></div>)
+        : (checkGameOwner && (
+          <div className="text-center"><a className="btn btn-success auction-link text-white" role="button" aria-pressed="true" onClick={() => postAuction(game.id)}>Post
+            auction</a></div>))}
     </div>
   </div>
 );
