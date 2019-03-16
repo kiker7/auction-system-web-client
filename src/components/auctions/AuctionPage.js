a0f653216c13f9aa2c4fe4660f39993ea33183de
rrutyna <rafalrutyna00@gmai.com>
2019-03-16 20:18:54 +0100

    Update AuctionPage component.

--- src/components/auctions/AuctionPage.js
+++ src/components/auctions/AuctionPage.js
@@ -1,30 +1,69 @@
 import React from 'react';
 import {connect} from "react-redux";
 import PropTypes from 'prop-types';
+import * as actions from "../../actions/auctionActions";
+import {bindActionCreators} from "redux";
+import {toastError} from "../../utils/errors";
 
 
-class AuctionPage extends React.Component{
+class AuctionPage extends React.Component {
 
   componentDidMount() {
-    console.log(this.props.match.params.id); // eslint-disable-line no-console
+    if (this.props.auctions.length === 0) {
+      this.props.actions.loadAuctions()
+        .catch(error => {
+          toastError("Failed to load auctions.", error)
+        });
+    }
   }
 
+  followAuction = () => {
+    console.log("follow auction"); // eslint-disable-line no-console
+  };
+
   render() {
+
+    const auction = this.props.auctions.find(auction => {
+      return auction.id === this.props.match.params.id;
+    });
+
     return (
-      <div>Auction Page</div>
+      <div className="container">
+        {auction !== undefined &&
+        (<div className="text-center auction-header">
+          <div className="d-flex">
+            <div className="mr-auto"><h4>{auction.game.name}</h4><p>Price: {auction.game.price} $</p></div>
+            <div className="auction-card-closing-time"><a className="btn btn-success text-white"
+                                                          onClick={this.followAuction}>Follow</a></div>
+          </div>
+          <div>
+            post bid
+          </div>
+          <div>
+            bids
+          </div>
+        </div>)}
+      </div>
     );
   }
 }
 
 AuctionPage.propTypes = {
-  username: PropTypes.string,
-  match: PropTypes.object.isRequired
+  match: PropTypes.object.isRequired,
+  actions: PropTypes.object.isRequired,
+  auctions: PropTypes.array.isRequired
 };
 
 function mapStateToProps(state) {
   return {
-    username: state.userStore.currentUser.username,
+    auctions: state.auctionStore.auctions
   };
 }
 
-export default connect(mapStateToProps)(AuctionPage);
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AuctionPage);
--- src/reducers/auctionReducer.js
+++ src/reducers/auctionReducer.js
@@ -5,6 +5,8 @@ export default function auctions(state = initialState.auctionStore, action) {
   switch (action.type) {
     case types.LOAD_AUCTIONS_SUCCESS:
       return {...state, auctions: action.auctions};
+    case types.LOAD_AUCTION_SUCCESS:
+      return {...state};
     default:
       return state;
   }
